60cd8cbe5ac1412463fa26f51d1f52b34fd3df08
lalitchandra00 <mail4lalitchandra@gmail.com>
2026-06-26 17:00:03 +0530

    fixing bugs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 import mongoose from 'mongoose';
+
+import dns from 'node:dns';
+// Fix for Node.js DNS resolution issues on some Windows machines
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
 import {DB_NAME} from './constants.js';
 import dotenv from 'dotenv';
 dotenv.config({
@@ -18,7 +23,7 @@ async function connectDB(){
         })
 
         app.listen(process.env.PORT, () => {
-            console.log(`App is listening on port ${process.env.PORT}`);
+            console.log(`Database connected successfully! and App is listening on port ${process.env.PORT}`);
         })
     }
     catch (error){
